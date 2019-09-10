4356fcf2e98ff2d83b25c7741390e644cebbe4af
Takumi Ishikawa <takumiishikawa94@gmail.com>
2019-09-10 19:23:15 +0900

    add debug function

--- header.hpp
+++ header.hpp
@@ -20,6 +20,31 @@
 #include <tuple>
 #include <vector>
 #define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+//#define cerr if(false) cerr
+#ifdef DEBUG
+#define show(...) cerr << #__VA_ARGS__ << " = ", debug(__VA_ARGS__);
+#else
+#define show(...) 42
+#endif
 using namespace std;
 using ll = long long;
 using pii = pair<int, int>;
+template <typename T, typename S>
+ostream& operator<<(ostream& os, pair<T, S> a) {
+    os << '(' << a.first << ',' << a.second << ')';
+    return os;
+}
+template <typename T>
+ostream& operator<<(ostream& os, vector<T> v) {
+    for (auto x : v) os << x << ' ';
+    return os;
+}
+void debug() {
+    cerr << '\n';
+}
+template <typename H, typename... T>
+void debug(H a, T... b) {
+    cerr << a;
+    if (sizeof...(b)) cerr << ", ";
+    debug(b...);
+}:
