5f9861df38632d98796ec79ba0e64acf0da82c6f
Takumi Ishikawa <takumiishikawa94@gmail.com>
2019-09-12 11:33:47 +0900

    add manacher

---
+++ string/manacher.hpp
@@ -0,0 +1,26 @@
+vector<int> manacher(const string& s){
+    int n = (int) s.size();
+    vector<int> res(n);
+    for(int i = 0, j = 0; i < n;){
+        while(i - j >= 0 and i + j < n and s[i - j] == s[i + j])j++;
+        res[i] = j;
+        int k = 1;
+        while(i - k >= 0 and i + k < n and k + res[i - k] < j){
+            res[i + k] = res[i - k];
+            k++;
+        }
+        i += k;
+        j -= k;
+    }
+    return res;
+}
+int longest_palindrome(const string &s){
+    string t;
+    t += '#';
+    for(char c : s){
+        t += c;
+        t += '#';
+    }
+    auto d = manacher(t);
+    return *max_element(d.begin(), d.end()) - 1;
+}
