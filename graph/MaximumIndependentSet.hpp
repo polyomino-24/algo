dc13b7c6471be4083aa196fed790dc213dba968f
Takumi Ishikawa <takumiishikawa94@gmail.com>
2019-10-24 23:33:23 +0900

    復元ありに変更

--- graph/MaximumIndependentSet.hpp
+++ graph/MaximumIndependentSet.hpp
@@ -2,7 +2,7 @@
 //dp[i] = i (\subseteq S1)による誘導部分グラフのMIS
 //f[i] =  S1からi (\in S2)につながっている頂点を削除した集合
 template<typename T = int>
-T MaximumIndependentSet(const vector<vector<int>> &g, vector<T>cost = {}){
+vector<int> MaximumIndependentSet(const vector<vector<int>> &g, vector<T>cost = {}){
     const int n = (int) g.size();
     if(cost.size() == 0) cost = vector<T>(n, 1);
     assert(cost.size() == n);
@@ -15,13 +15,6 @@ T MaximumIndependentSet(const vector<vector<int>> &g, vector<T>cost = {}){
             bit[i] |= 1LL << x;
         }
     }
-    //本来これだけど、無理やり1つにまとめると速そう
-    // for(int i = 1; i < 1 << half; i++){
-    //     for(int j = 0; j < half; j++){
-    //         if(!(i >> j & 1))continue;
-    //         dp[i] = max(dp[i], dp[i & (~bit[j])] + cost[j]);
-    //     }
-    // }
     vector<int> f(1 << half2);
     f[0] = (1 << half) - 1;
     for(int i = 1; i < 1 << half2; i++){
@@ -34,8 +27,30 @@ T MaximumIndependentSet(const vector<vector<int>> &g, vector<T>cost = {}){
         f[i] = f[i ^ (1 << j)] & (~bit[j + half]);
     }
     T res = 0;
+    int id = -1;
     for(int i = 0; i < 1 << half2; i++){
-        res = max(res, dp2[i] + dp[f[i]]);
+        if(res < dp2[i] + dp[f[i]]){
+            res = dp2[i] + dp[f[i]];
+            id = i;
+        }
     }
-    return res;
+//    return res;
+    int id2 = -1;
+    for(int i = 0; i < 1 << half2; i++){
+        if((i|(f[id])) == f[id] and dp[i] == dp[f[id]]){
+            id2 = i;
+            break;
+        }
+    }
+    vector<int> p;
+    for(int i = 0; i < half2; i++){
+        if(id >> i & 1){
+            p.push_back(i + half);
+        }
+        if(id2 >> i & 1){
+            p.push_back(i);
+        }
+    }
+    assert(res == int(p.size()));
+    return p;
 }
