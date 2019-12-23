7b97ba709d302599aa30d4342c279b1b8479e65b
Takumi Ishikawa <takumiishikawa94@gmail.com>
2019-12-23 16:10:03 +0900

    add Trie

---
+++ string/Trie.hpp
@@ -0,0 +1,33 @@
+template<int SZ = 26>
+struct Trie{
+    struct Node{
+        Node *next[SZ];
+        Node(){
+            for(int i = 0; i < SZ; i++){
+                next[i] = nullptr;
+            }
+        }
+    };
+    using NP = Node*;
+    NP root;
+    Trie(){
+        root = new Node();
+    }
+    void add(const string &s){
+        NP now = root;
+        for(const char c : s){
+            int i = c - 'a';//要変更
+            if(!now -> next[i]){
+                now -> next[i] = new Node();
+            }
+        }
+    }
+    void query(NP *now){
+        for(int i = 0; i < SZ; i++){
+            if(now -> next[i]){
+                query(now -> next[i]);
+            }
+        }
+    }
+};
+
